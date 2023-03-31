67c59185cbb0b967c6cb0129fcee6bdea6c7046f
Serg Shutin <35927843+saldius@users.noreply.github.com>
2023-04-01 00:31:38 +0900

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -2,3 +2,11 @@ def string_upper(s):
     """возвращает строку со всеми заглавными буквами"""
     return s.upper()
 print(string_upper("я играю на гармошке у прохожих "))
+
+def changeling( ) :
+    ''' переворачивает слово наоборот'''
+    word = input('Введите слово: ')
+    return print(word[::-1])
+
+changeling()
+
